7de654595e389c5b6a6a155ccd0e77718a8af442
Neil Bauman <nbauman@rocketmail.com>
2025-11-09 12:37:29 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#003f5c",
+        accent: "#ffa600",
+      },
+    },
   },
   plugins: [],
-}
+};
