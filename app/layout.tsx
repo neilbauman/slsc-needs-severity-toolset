0e4864cc835eb59678009c7a9c64d5b053db3426
Neil Bauman <nbauman@rocketmail.com>
2025-11-09 19:25:40 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,11 +6,7 @@ export const metadata = {
   description: "Philippines SSC data and decision support system",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="bg-gray-50 text-gray-800">
