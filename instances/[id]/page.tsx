34074c14185e395fe3412a0f77363a926b59a428
Neil Bauman <nbauman@rocketmail.com>
2025-11-09 10:51:22 +0800

    Delete instances/[id] directory

--- instances/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function InstanceDetailPage() {
-  return (
-    <main className="p-6">
-      <h1 className="text-2xl font-bold">Instance Detail</h1>
-      <p className="text-sm text-gray-600">Data, map overlays, and metrics for selected instance.</p>
-    </main>
-  );
-}
