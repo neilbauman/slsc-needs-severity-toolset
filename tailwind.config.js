c8ea0c19b04b5e165c09dfeb24f169f51b97c54b
Neil Bauman <nbauman@rocketmail.com>
2025-11-09 12:52:02 +0800

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: "#003f5c",
-        accent: "#ffa600",
-      },
+        primary: "#153e56",
+        accent: "#f6a623"
+      }
     },
   },
   plugins: [],
-};
+}
