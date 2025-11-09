f551059fbf0cf8f2a12f38983ee4cf50faa25bd6
Neil Bauman <nbauman@rocketmail.com>
2025-11-09 11:42:00 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,5 @@
 // app/layout.tsx
-
-
-export const metadata = {
-  title: 'Philippines SSC Toolset',
-  description: 'Simple working app to verify deployment',
-};
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
