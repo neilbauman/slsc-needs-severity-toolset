7172545bcdf4625d6e32a80aee0374d659fffdc4
Neil Bauman <nbauman@rocketmail.com>
2025-11-10 21:23:14 +0800

    Create supabaseBrowser.ts

---
+++ lib/supabaseBrowser.ts
@@ -0,0 +1,12 @@
+// lib/supabaseBrowser.ts
+'use client';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+export function createClientBrowser() {
+  return createClient(supabaseUrl, supabaseAnonKey, {
+    auth: { persistSession: false, autoRefreshToken: false },
+  });
+}
