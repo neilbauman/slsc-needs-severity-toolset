c0ad6a60926171f4d0508270e997d86b6aeaf645
Neil Bauman <nbauman@rocketmail.com>
2025-11-11 08:29:49 +0800

    Update page.tsx

--- app/instances/page.tsx
+++ app/instances/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { createClient } from '@/lib/supabase/supabaseClient';
+import { createClient } from '@/lib/supabaseClient';
 import InstanceRecomputePanel from '@/components/InstanceRecomputePanel';
 
 type Instance = {
